974b8e9efd45f5ec10796b9e5fc7721883c9e7f9
mo <mohanad.mazin188@hotmail.com>
2024-05-19 23:59:12 -0400

    Model inference

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,7 +1,12 @@
-from ultralytics import YOLO
+from roboflow import Roboflow
+import os
 
-model = YOLO("models/best.pt")
-model.predict(
-    "input_pictures/download (1).jpeg",
-    save=True,
-)
+roboflow_api_key=os.environ.get("ROBOFLOW_API_KEY")
+
+rf = Roboflow(api_key=roboflow_api_key)
+project = rf.workspace().project("dog-breeds-142de")
+model = project.version(2).model
+
+result = model.predict("/home/mo/projects/dog_breed_analysis/1280px-003_American_Pit_Bull_Terrier.jpg", confidence=40, overlap=30).json()
+result
+most_confident_class = result["predictions"][0]["class"].capitalize()
